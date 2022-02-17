bd2d80fb612c332d447f434cece14a8393e2a3ed
Daichi Ohishi <daichiohishi@gmail.com>
2022-02-17 02:12:32 +0000

    Update user edit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,13 @@
 class ApplicationController < ActionController::Base
   before_action :set_q
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
 
   def set_q
     @q = Post.ransack(params[:q])
