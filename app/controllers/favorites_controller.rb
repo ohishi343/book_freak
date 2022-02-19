84f5e58b9eb01b6e441d81a1b309e26162fac878
Daichi Ohishi <daichiohishi@gmail.com>
2022-02-19 12:16:31 +0000

    Update Views

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,8 +2,8 @@ class FavoritesController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    favorites = Favorite.where(user_id: current_user.id).pluck(:post_id).paginate(page: params[:page], per_page: 15)
-    @posts = Post.includes(:user).find(favorites)
+    favorite = Favorite.where(user_id: current_user).pluck(:post_id)
+    @posts = Post.where(id: favorite).includes(:user).order(created_at: :desc).paginate(page: params[:page], per_page: 15)
   end
 
   def create
