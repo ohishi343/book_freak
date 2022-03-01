93629810db92abf7ac7ab63bffca87770ea01179
Daichi Ohishi <daichiohishi@gmail.com>
2022-03-01 04:20:27 +0000

    Update some views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,6 +11,7 @@ class PostsController < ApplicationController
 
   # GET /posts/1 or /posts/1.json
   def show
+    @book = RakutenWebService::Books::Book.search(title: @post.title, author: @post.author, hits: 1)
     @books = RakutenWebService::Books::Book.search(title: @post.title, hits: 3)
   end
 
